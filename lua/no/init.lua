c7ecaa22db4f48a61e23670d5eb668abb1f463ec
fholmer <fholmer@gmail.com>
2025-01-01 07:22:55 +0100

    Update init.lua
    Fix the øø and ææ keybinding for lazyvim. added ¨ to / binding.

--- lua/no/init.lua
+++ lua/no/init.lua
@@ -1,12 +1,15 @@
 local M = {}
 
 function M.setup()
-    vim.keymap.set("n", "ø", "[")
-    vim.keymap.set("n", "øø", "[[")
-    vim.keymap.set("n", "æ", "]")
-    vim.keymap.set("n", "ææ", "]]")
     vim.keymap.set("n", "Ø", "{")
     vim.keymap.set("n", "Æ", "}")
+
+    vim.keymap.set({ "n", "o", "v" }, "ø", "[", { remap = true, desc = "[next" })
+    vim.keymap.set({ "n", "o", "v" }, "æ", "]", { remap = true, desc = "]next" })
+    vim.keymap.set({ "n", "v", "o" }, "]æ", "]]", { remap = true })
+    vim.keymap.set({ "n", "v", "o" }, "[ø", "[[", { remap = true })
+    vim.keymap.set({ "n", "v", "o", "i" }, "¨", "/")
+    
 end
 
 return M
